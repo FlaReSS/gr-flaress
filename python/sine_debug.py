08c7565ea0fcf6365f9ecf7cd762369e4ffd9723
ISISPACE <a.miraglia@isispace.nl>
2018-10-31 13:56:47 +0100

    V15 new algorithm sine debug

--- python/sine_debug.py
+++ python/sine_debug.py
@@ -4,36 +4,29 @@
 # Copyright 2018 Antonio Miraglia - ISISpace .
 #
 
-import numpy, math
+import numpy
 from gnuradio import gr
-
+import numpy as np
 
 class sine_debug(gr.sync_block):
     """
-    docstring for block sine_debug
+    this block essentially evaluates the derivative of phase (Arg) of input signal. This calculus is used to check if the input sine wave has phase jumps, if not, the output will be a costant.
     """
     def __init__(self):
         gr.sync_block.__init__(self,
             name="sine_debug",
             in_sig=[numpy.complex64],
-            out_sig=None)
+            out_sig=[numpy.float32])
+        self.d_in = 0
 
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
+        out0 = output_items[0]
         d_phase = 0
         for i in range(len(in0)):
-            phase = math.atan2(in0[i].imag, in0[i].real)
-
-            if (i == 0):
-                step = abs(phase)
-
-            if (abs(d_phase) + step) <= math.pi:
-                if abs(phase - d_phase) - step > 0.1:
-                    print "WARNING: jump phase of:",abs(phase - d_phase)
-                    step = abs(phase - d_phase)
-
-            d_phase = phase
-
+            temp = in0[i] * np.conjugate(self.d_in)
+            out0[i] = np.arctan2(temp.imag, temp.real)
+            self.d_in = in0[i]
         return len(input_items[0])
 
